bc769f14bf464b9ccbc859837217ea3ab9eb0c1e
MalinavyBusel <egorbgtrvch@gmail.com>
2023-09-25 10:26:30 +0300

    added reading id from file and writing to it. implemented some staff for retries

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"github.com/amarnathcjd/gogram/telegram"
 	"github.com/joho/godotenv"
 	"github.com/k0kubun/pp"
+	"io"
 	"mangaParser/config"
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 var titles = []string{"Жрец порчи"}
@@ -36,39 +38,77 @@ func main() {
 		panic(err)
 	}
 
-	var lastId int = 9290 // TODO read from file
-	ReadLastId()
+	lastId, err := ReadLastId("post_id.txt")
+	if err != nil {
+		panic(err)
+	}
 	for {
-		fmt.Println(lastId)
-		posts, postErr := client.GetMessages(conf.Channel, &telegram.SearchOption{IDs: lastId})
-		fmt.Println("-----")
-		if postErr != nil {
-			pp.Println(postErr) // TODO log in file
+		ch := make(chan string)
+		go func() {
+			defer close(ch)
+			posts, postErr := client.GetMessages(conf.Channel, &telegram.SearchOption{IDs: lastId})
+			msg := ""
+
+			if postErr != nil {
+				pp.Println(postErr) // TODO log in file
+				ch <- "error"
+				return
+			}
+			post := posts[0].Message
+
+			if post.Message != "" && Contains(post.Message, titles) {
+				r, _ := regexp.Compile("https://teletype.in/@lrmanga/[^\x1b]+")
+				t := r.FindString(pp.Sprint(post.ReplyMarkup))
+				msg = t
+			}
+
+			ch <- msg
+		}()
+
+		select {
+		case msg := <-ch:
+			fmt.Println("Received", lastId, " - ", msg)
+			if msg == "" {
+				fmt.Println("Finishing at", lastId)
+				if err := WriteNewLastId("post_id.txt", lastId); err != nil {
+					panic(err)
+				}
+			}
+			//} else if msg != "error" {
+			//	fmt.Println(msg)
+			//}
+
+		case <-time.After(5 * time.Second):
+			fmt.Println("Timed out", lastId)
 			continue
 		}
-		post := posts[0].Message
-		if post.Message == "" {
-			fmt.Println("FINISH")
-			WriteNewLastId()
-			os.Exit(0)
-		} else {
-			lastId++
-		}
-		//pp.Println(post.Message)
-		if Contains(post.Message, titles) {
-			r, _ := regexp.Compile("https://teletype.in/@lrmanga/[^\x1b]+")
-			t := r.FindString(pp.Sprint(post.ReplyMarkup))
-			pp.Println(t)
-		}
+		lastId++
+
 	}
 
-	//p, _ := client.GetSendablePeer("@prbezposhady")
-	//pp.Println(client.MessagesGetHistory(&telegram.MessagesGetHistoryParams{Peer: p, Limit: 10, OffsetID: -1}))
-	//.GetSendablePeer(PeerID)
 }
 
-func ReadLastId()     {}
-func WriteNewLastId() {}
+func ReadLastId(filename string) (int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+	var number int
+	if _, err := fmt.Fscanf(file, "%d\n", &number); err != nil && err != io.EOF {
+		return 0, err
+	}
+	return number, nil
+}
+func WriteNewLastId(filename string, id int) error {
+	file, err := os.OpenFile(filename, os.O_TRUNC|os.O_WRONLY, 0777)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.WriteString(fmt.Sprintf("%d", id))
+	return err
+}
 func Contains(v string, arr []string) bool {
 	for _, el := range arr {
 		if strings.Contains(v, el) {
